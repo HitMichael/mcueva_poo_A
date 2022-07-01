dd7d432f9d0c7ce143959d67117c4a779bd131ba
HP <HP@Michael>
2022-07-01 01:36:50 -0500

    Mi primer Commit 2022_Programación Orientada a Objetos_INT_2do "A"

---
+++ src/ec/edu/intsuperior/modelo/Persona.java
@@ -0,0 +1,70 @@
+package ec.edu.intsuperior.modelo;
+
+public class Persona {
+
+    String cedula;
+    String nombre;
+    String apellido;
+    String dirección;
+
+    private static class TipoGenero {
+
+        public TipoGenero() {
+        }
+    }
+
+    public enum genero {
+        MACULINO, FEMENINO
+    }
+    TipoGenero genero;
+
+    public Persona() {
+
+    }
+
+    public Persona(String cedula, String nombre, String apellido, String dirección, TipoGenero genero) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dirección = dirección;
+        this.genero = genero;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDirección() {
+        return dirección;
+    }
+
+    public void setDirección(String dirección) {
+        this.dirección = dirección;
+    }
+
+    @Override
+    public String toString() {
+        return "Los datos solicitados de la persona son:" + "Cedula: " + getCedula() + "Nombre: " + getNombre()
+                + "Apellido: " + getApellido() + "Dirección" + getDirección();
+    }
+}
---
+++ src/ec/edu/intsuperior/vista/Aplicación.java
@@ -0,0 +1,42 @@
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Persona;
+import java.util.Scanner;
+
+public class Aplicación {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        Persona p1 = new Persona();
+        Persona p2 = new Persona();
+        Persona p3 = new Persona();
+        System.out.print("Ingrese el número de cedula de la persona 1 por favor:  ");
+        p1.setCedula(leer.next());
+        System.out.print("Ingrese el nombre de la persona 1 por favor:  ");
+        p1.setNombre(leer.next());
+        System.out.print("Ingrese el apellido de la persona 1 por favor:  ");
+        p1.setApellido(leer.next());
+        System.out.print("Ingrese la dirección de la persona 1 por favor:   ");
+        p1.setDirección(leer.next());
+
+        System.out.println(p1.toString());  
+        
+        System.out.print("Los datos solicitados de la persona 1 son:  \n"
+                + "Cedula:  " + p1.getCedula() + "\n"
+                + "Nombre y Apeliido:  " + p1.getNombre() + " " + p1.getApellido() + "\n"
+                + "Dirección:  " + p1.getDirección() + "\n");
+
+        
+        System.out.print("Ingrese el número de cedula de la persona 2 por favor:");
+        p2.setCedula(leer.next());
+        System.out.print("Ingrese el nombre de la persona 2 por favor:");
+        p2.setNombre(leer.next());
+        System.out.print("Ingrese el apellido de la persona 2 por favor:  ");
+        p2.setApellido(leer.next());
+        System.out.print("Ingrese la dirección de la persona2 por favor:");
+        p2.setDirección(leer.next());
+
+
+    }
+
+}
